173824579a595fd592b0f8278c67007758e5fccc
abobak <abobak@szklacom>
2017-06-08 00:51:20 +0200

    Dodanie pobrania drużyn wraz z kadrą. Dodanie tabeli z meczami na żywo.

--- js/ApiFootballData.js
+++ js/ApiFootballData.js
@@ -21,6 +21,15 @@ var AFD = {
             case 'fixtures':
                 this.ajaxRequest('matches', this.showCompetitionFixtures);
                 break;
+            case 'teams':
+                this.ajaxRequest('teams', this.showCompetitionTeams);
+                break;
+            case 'team':
+                this.ajaxRequest('team', this.showTeam);
+                break;
+            case 'players':
+                this.ajaxRequest('players', this.showSquad);
+                break;
             default:
                 $("#main").show();
                 $("#loader").hide();
@@ -29,9 +38,8 @@ var AFD = {
     },
 
     ajaxRequest: function(name, callback) {
-        $("#main").hide();
+        $("#loader ~ div").hide();
         $("#loader").show();
-        var afd = this;
         $.ajax({
             url: historyActions.getLastAction().url,
             type: 'GET',
@@ -40,16 +48,6 @@ var AFD = {
             success: function(data) {
                 callback(name, data);
                 $("#loader").hide();
-                $("#stats #"+name+" .row:nth-child(n+2)").on('click', function(){
-                    historyActions.addHistoryAction(
-                        new historyObject(
-                            $(this).find('input').val(),
-                            Date.now(),
-                            $(this).find('input').data('name')
-                        )
-                    );
-                    afd.getStatistics(name);
-                });
             },
             error: function() {
                 console.log('Failed!');
@@ -63,7 +61,7 @@ var AFD = {
             numberOfTeams: 'Liczba drużyn',
             lastUpdated: 'Ostatnia aktualizacja'
         };
-        var content = '<div id="'+name+'" class="container col-md-8 col-md-offset-2">';
+        var content = '<div id="'+name+'" class="container col-md-8 col-md-offset-2 stats_container">';
         content += '<div class="row">';
         content += '<div class="col-sm-6">Nazwa rozgrywek</div>';
         content += '<div class="col-sm-3 hidden-xs">Liczba drużyn</div>';
@@ -80,23 +78,34 @@ var AFD = {
         content += '</div>';
 
         $("#stats").empty().append(content).show();
+
+        $("#competitionsInfo .row:nth-child(n+2)").on('click', function(){
+            historyActions.addHistoryAction(
+                new historyObject(
+                    $(this).find('input').val(),
+                    Date.now(),
+                    $(this).find('input').data('name')
+                )
+            );
+            AFD.getStatistics(name);
+        });
     },
 
     showCompetitionInfo: function(name, data) {
-        var content = '<div id="'+name+'" class="container col-md-4 col-md-offset-4">';
+        var content = '<div class="container col-md-4 col-md-offset-4 stats_container">';
         content += '<div class="row">';
         content += '<div><h3>' + data.caption + '</h3></div>';
         content += '</div>';
 
         if (data.currentMatchday == data.numberOfMatchdays) {
-            content += '<div class="row"><div id="table" data-url="' + data._links.leagueTable.href + '" data-name="'+ data.caption +' - Tabela"><span class="glyphicon glyphicon-list hidden-xs" aria-hidden="true"></span> Tabela</div></div>';
+            content += '<div class="row"><div id="competitions" data-url="' + data._links.leagueTable.href + '" data-name="'+ data.caption +' - Tabela"><span class="glyphicon glyphicon-list hidden-xs" aria-hidden="true"></span> Tabela</div></div>';
         }
         content += '<div class="row"><div id="fixtures" data-url="' + data._links.fixtures.href + '" data-name="'+ data.caption +' - Mecze"><span class="glyphicon glyphicon-certificate hidden-xs" aria-hidden="true"></span> Mecze</div></div>';
         content += '<div class="row"><div id="teams" data-url="' + data._links.teams.href + '" data-name="'+ data.caption +' - Drużyny"><span class="glyphicon glyphicon-user hidden-xs" aria-hidden="true"></span> Drużyny</div></div>';
 
         $("#stats").empty().append(content).show();
 
-        $("#table").on("click", function(){
+        $("#competitions, #fixtures, #teams").on("click", function(){
             historyActions.addHistoryAction(
                 new historyObject(
                     $(this).data('url'),
@@ -104,18 +113,7 @@ var AFD = {
                     $(this).data('name')
                 )
             );
-            AFD.getStatistics('competitions');
-        });
-
-        $("#fixtures").on("click", function(){
-            historyActions.addHistoryAction(
-                new historyObject(
-                    $(this).data('url'),
-                    Date.now(),
-                    $(this).data('name')
-                )
-            );
-            AFD.getStatistics('fixtures');
+            AFD.getStatistics($(this).attr('id'));
         });
     },
 
@@ -126,7 +124,7 @@ var AFD = {
         content += '<button id="btnTableHome" type="button" class="btn btn-default">Statystyki domowe</button>';
         content += '<button id="btnTableAway" type="button" class="btn btn-default">Statystyki wyjazdowe</button>';
         content += '</div>';
-        content += '<div class="container col-md-8 col-md-offset-2 standing">';
+        content += '<div class="container col-md-8 col-md-offset-2 standing stats_container">';
         content += '<div class="row">';
         content += '<div class="col-sm-1 col-xs-2">Poz.</div>';
         content += '<div class="col-sm-4 col-xs-8">Drużyna</div>';
@@ -149,7 +147,7 @@ var AFD = {
             content += '<div class="col-sm-1 hidden-xs"><span class="full">' + item.goals + '</span><span class="home">' + item.home.goals + '</span><span class="away">' + item.away.goals + '</span></div>';
             content += '<div class="col-sm-1 hidden-xs"><span class="full">' + item.goalsAgainst + '</span><span class="home">' + item.home.goalsAgainst + '</span><span class="away">' + item.away.goalsAgainst + '</span></div>';
             content += '<div class="col-sm-1 col-xs-2"><span class="full">' + item.points + '</span><span class="home">' + (item.home.wins*3+item.home.draws) + '</span><span class="away">' + (item.away.wins*3+item.away.draws) + '</span></div>';
-            content += '<input type="hidden" value="'+ item['_links']['team']['href'] +'">';
+            content += '<input type="hidden" value="'+ item['_links']['team']['href'] +'" data-name="'+item.teamName+'">';
             content += '</div>'
         });
         content += '</div>';
@@ -173,6 +171,17 @@ var AFD = {
             $(".full, .home").hide();
             $(".away").show();
         });
+
+        $(".standing .row:nth-of-type(n+2)").on("click", function() {
+            historyActions.addHistoryAction(
+                new historyObject(
+                    $(this).find("input").val(),
+                    Date.now(),
+                    $(this).find("input").data("")
+                )
+            );
+            AFD.getStatistics('team');
+        });
     },
 
     showCompetitionFixtures: function(name, data) {
@@ -181,13 +190,18 @@ var AFD = {
         });
         var currentMatchday = 0;
         content = '';
+        var liveMatches = Array();
         $.each(data.fixtures, function (i, item) {
+            if (item.status == 'IN_PLAY') {
+                var index = liveMatches.length;
+                liveMatches[index] = i;
+            }
             if (data['fixtures'][i].matchday != currentMatchday) {
                 currentMatchday = data['fixtures'][i].matchday;
                 if (currentMatchday > 1) {
                     content += '</div>';
                 }
-                content += '<div class="container col-md-8 col-md-offset-2 fixtures">';
+                content += '<div class="container col-md-8 col-md-offset-2 fixtures stats_container">';
                 content += '<div class="row">';
                 content += '<div class="col-md-1 hidden-sm hidden-xs">' + currentMatchday + '</div>';
                 content += '<div class="col-md-3 col-xs-6">Gospodarz</div>';
@@ -200,9 +214,11 @@ var AFD = {
             content += '<div class="row">';
 
             content += '<div class="col-md-1 hidden-sm hidden-xs">&nbsp;</div>';
-            content += '<div class="col-md-3 col-xs-6">' + item.homeTeamName + '</div>';
+            content += '<div class="col-md-3 col-xs-6 clubName">' + item.homeTeamName + '</div>';
+            content += '<input type="hidden" value="' + item['_links']['homeTeam']['href'] + '" data-name="'+item.homeTeamName+'">';
             content += '<div class="col-md-1 hidden-sm hidden-xs">vs.</div>';
-            content += '<div class="col-md-3 col-xs-6">' + item.awayTeamName + '</div>';
+            content += '<div class="col-md-3 col-xs-6 clubName">' + item.awayTeamName + '</div>';
+            content += '<input type="hidden" value="' + item['_links']['awayTeam']['href'] + '" data-name="'+item.awayTeamName+'">';
 
             var extraTime = '';
             if (item.result.extraTime != undefined) {
@@ -220,24 +236,103 @@ var AFD = {
 
         $("#stats").empty().append(content).show();
 
-        $(".tableType").on('click', function() {
-            $(".tableType").removeClass('tableActive')
-            $(this).addClass('tableActive');
+        content = '<div class="container col-md-8 col-md-offset-2 fixtures">';
+        $.each(liveMatches, function (i, item) {
+            content += '<div class="row">';
+            content += '<div class="col-xs-3 ">! NA ŻYWO !</div>';
+            content += '<div class="col-xs-4">'+data.fixtures[item]['homeTeamName']+'</div>';
+            content += '<div class="col-xs-1">vs.</div>';
+            content += '<div class="col-xs-4">'+data.fixtures[item]['awayTeamName']+'</div>';
+            content += '</div>';
+            console.log(data.fixtures[item])
         });
+        content += '</div>';
+        if (liveMatches.length>0) {
+            $("#stats").prepend(content);
+        }
 
-        $("#table").on("click", function(){
-            $("#leagueTable .home, #leagueTable .away").hide();
-            $("#leagueTable .full").show();
+        $(".clubName").on("click", function(){
+            historyActions.addHistoryAction(
+                new historyObject(
+                    $(this).find("~input").val(),
+                    Date.now(),
+                    $(this).find("~input").data("name")
+                )
+            );
+            AFD.getStatistics("team");
         });
+    },
 
-        $("#tableHome").on("click", function(){
-            $("#leagueTable .full, #leagueTable .away").hide();
-            $("#leagueTable .home").show();
+    showCompetitionTeams: function(name, data) {
+        var content = '<h3 class="col-md-8 col-md-offset-2">' + historyActions.getLastAction().name + '</h3>';
+        content += '<div class="container col-md-8 col-md-offset-2 ">';
+        content += '<div class="row teams">';
+        $.each(data.teams, function (i, item) {
+            content += '<div class="col-sm-6 col-xs-12 col-md-4">' + item.name + '</div>';
+            content += '<input type="hidden" value="' + item['_links']['self']['href'] + '" data-name="'+item.name+'">';
         });
+        content += '</div>';
 
-        $("#tableAway").on("click", function(){
-            $("#leagueTable .full, #leagueTable .home").hide();
-            $("#leagueTable .away").show();
+        $("#stats").empty().append(content).show();
+
+        $(".teams div").on("click", function(){
+            historyActions.addHistoryAction(
+                new historyObject(
+                    $(this).find('~ input').val(),
+                    Date.now(),
+                    $(this).find('~ input').data('name')
+                )
+            );
+            AFD.getStatistics('team');
         });
+    },
+
+    showTeam: function(name, data) {
+        var content = '<div class="container col-md-4 col-md-offset-4 team stats_container">';
+
+        content += '<div class="row">';
+        content += '<div>';
+        if (data.crestUrl != null) {
+            content += '<img src="' + data.crestUrl +'" />';
+        }
+        content += '<h3>' + data.name + '</h3></div>';
+        content += '</div>';
+
+        content += '<div class="row"><div id="fixtures" data-url="' + data._links.fixtures.href + '" data-name="'+ data.name +' - Mecze"><span class="glyphicon glyphicon-certificate hidden-xs" aria-hidden="true"></span> Mecze</div></div>';
+        content += '<div class="row"><div id="players" data-url="' + data._links.players.href + '" data-name="'+ data.name +' - Skład"><span class="glyphicon glyphicon-user hidden-xs" aria-hidden="true"></span> Skład</div></div>';
+
+        $("#stats").empty().append(content).show();
+
+        $("#fixtures, #players").on("click", function(){
+            historyActions.addHistoryAction(
+                new historyObject(
+                    $(this).data('url'),
+                    Date.now(),
+                    $(this).data('name')
+                )
+            );
+            AFD.getStatistics($(this).attr('id'));
+        });
+    },
+
+    showSquad: function(name, data) {
+        var content = '<div class="container col-md-6 col-md-offset-3 team stats_container">';
+        content += '<div class="row">';
+        content += '<div><h3>' + historyActions.getLastAction().name + '</h3></div>';
+        content += '</div>';
+        if (data.players.length == 0) {
+            content += '<div class="row"><div class="col-xs-12">Brak informacji na temat piłkarzy</div></div>';
+        }
+        $.each(data.players, function (i, item) {
+            content += '<div class="row">';
+            content += '<div class="col-xs-1">' + item.jerseyNumber + '</div>';
+            content += '<div class="col-xs-7 col-sm-5">' + item.name + '</div>';
+            content += '<div class="col-xs-4 col-sm-3">' + item.nationality + '</div>';
+            content += '<div class="hidden-xs col-sm-3">' + item.dateOfBirth + '</div>';
+            content += '</div>';
+        });
+        content += '</div>';
+
+        $("#stats").empty().append(content).show();
     }
 };
